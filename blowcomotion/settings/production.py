b29a807511efe157152be55f00262099bf622c0d
Nick Pereira <nick@capitalcitycode.com>
2025-01-12 21:51:22 -0600

    Update production settings for static file compression and output style

--- blowcomotion/settings/production.py
+++ blowcomotion/settings/production.py
@@ -1,6 +1,10 @@
 from .base import *
 
 DEBUG = False
+COMPRESS_OFFLINE = True
+LIBSASS_OUTPUT_STYLE = 'compressed'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 
 try:
     from .local import *
