d79aada8e6799ef99882693137ded3b6dd09000f
Nick Pereira <nick@capitalcitycode.com>
2025-01-12 22:32:41 -0600

    Remove ManifestStaticFilesStorage from production settings for static file handling

--- blowcomotion/settings/production.py
+++ blowcomotion/settings/production.py
@@ -3,7 +3,6 @@ from .base import *
 DEBUG = False
 COMPRESS_OFFLINE = True
 LIBSASS_OUTPUT_STYLE = 'compressed'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 
 try:
