b1d88d669d7cceccd1f465e54a19e44063887023
Nick Pereira <nick@capitalcitycode.com>
2025-02-22 10:27:53 -0600

    crx build

--- blowcomotion/settings/dev.py
+++ blowcomotion/settings/dev.py
@@ -1,18 +1,22 @@
-from .base import *
+from .base import *  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-gpnmtm+eot&u_ki#&#sj7=^6*x0o!zlpf^qfnpq(&=qnx1etdz"
+SECRET_KEY = "st0$(st1e*ap!e+37nnw__rcc*0(mbi5yt22kfudw1yvu#@+9v"
 
-# SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS += [  # noqa
+    "django_sass",
+]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+WAGTAIL_CACHE = False
 
 try:
-    from .local import *
+    from .local import *  # noqa
 except ImportError:
     pass
