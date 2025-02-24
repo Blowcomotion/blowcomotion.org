7708c524d240a74f8b2e3aa99136893e60829e6c
Nick Pereira <nick@capitalcitycode.com>
2025-02-24 17:20:56 -0600

    Add EventsBlock for managing event details in Wagtail

--- blowcomotion/blocks.py
+++ blowcomotion/blocks.py
@@ -9,3 +9,14 @@ class HeroBlock(blocks.StructBlock):
         icon = 'image'
         label = 'Hero'
         template = 'blocks/hero_block.html'
+
+class EventsBlock(blocks.StructBlock):
+    title = blocks.CharBlock()
+    date = blocks.DateBlock()
+    location = blocks.CharBlock()
+    description = blocks.RichTextBlock()
+
+    class Meta:
+        icon = 'date'
+        label = 'Event'
+        template = 'blocks/events_block.html'
