1b6362573f8b0b09569dc7e75531398e6191bc9a
Nick Pereira <PresidentNick@users.noreply.github.com>
2026-02-03 19:18:17 -0600

    Add WAGTAILADMIN_BASE_URL to base and production settings

--- blowcomotion/settings/base.py
+++ blowcomotion/settings/base.py
@@ -212,3 +212,5 @@ WAGTAILIMAGES_IMAGE_MODEL = 'blowcomotion.CustomImage'
 
 GIGO_API_URL = "http://localhost:8000/api"
 GIGO_API_KEY = None
+
+WAGTAILADMIN_BASE_URL = 'http://localhost:8000'
--- blowcomotion/settings/production.py
+++ blowcomotion/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 LIBSASS_OUTPUT_STYLE = 'compressed'
-
+WAGTAILADMIN_BASE_URL = 'https://www.blowcomotion.org'
 
 try:
     from .local import *
